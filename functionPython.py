13bbadc013f225a39f7310ae8efefe1abef10203
Nguyen Tran <tranvinhnguyen@gmail.com>
2018-04-21 11:27:59 -0500

    function
    def abc(btc):
        method

---
+++ functionPython.py
@@ -0,0 +1,20 @@
+def beef():
+    print("this is an example of function in python")
+
+
+beef()
+
+def cow():
+    print('the function is cool')
+
+cow()
+cow()
+
+#convert bitcoin to usd
+def bitcoin_to_usd(btc):
+    amount = btc * 527
+    print(amount)
+
+print('\nconvert 10 bitcoin to usd')
+bitcoin_to_usd(10)
+
