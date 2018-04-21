fe6594424cc29dea29deaaaa44b9e165faa2a9c4
Nguyen Tran <tranvinhnguyen@gmail.com>
2018-04-21 11:54:43 -0500

    keywork argument

---
+++ keyword argument.py
@@ -0,0 +1,8 @@
+def dumb_sentence(name ='bucky', action='ate', item='tuna'):
+    print(name,action,item)
+
+dumb_sentence()
+dumb_sentence('vinh','drink', 'water')
+dumb_sentence('katie')
+dumb_sentence(action='throw')
+dumb_sentence(item='hems')
