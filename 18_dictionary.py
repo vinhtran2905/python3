78ebee1ed82d35135f4b9842d78b102413585601
Nguyen Tran <tranvinhnguyen@gmail.com>
2018-04-21 12:21:03 -0500

    dictionary
    words = {'key1':'value1','key2':'value2'}

---
+++ 18_dictionary.py
@@ -0,0 +1,8 @@
+classmates = {'tony':' cool but smell', 'emma':' she sits behind me', 'lucy': ' she asks too many questions'}
+
+print(classmates)
+print(classmates['tony'])
+
+for k,v in classmates.items():
+    print("\n")
+    print(k + v)
