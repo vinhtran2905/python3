ad5bc43e326da8c7adfd5198e836c91965a4f79f
Nguyen Tran <tranvinhnguyen@gmail.com>
2018-04-21 10:24:08 -0500

    String

---
+++ String.py
@@ -0,0 +1,21 @@
+text = " Vinh Tran"
+
+print(text)
+
+text1 = 'First name'
+
+print(text1)
+
+
+str1 = "I don't eat"
+
+print(str1)
+
+
+str2 = "she said \" what is the my favourite coloe\""
+
+print(str2)
+
+str3 = "new line \n a new line begins"
+
+print(str3)
