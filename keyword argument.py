dc6248ba9a109b585ec34d25c23fb698155fc770
Nguyen Tran <tranvinhnguyen@gmail.com>
2018-04-21 12:10:37 -0500

    change name

--- keyword argument.py
+++
@@ -1,8 +0,0 @@
-def dumb_sentence(name ='bucky', action='ate', item='tuna'):
-    print(name,action,item)
-
-dumb_sentence()
-dumb_sentence('vinh','drink', 'water')
-dumb_sentence('katie')
-dumb_sentence(action='throw')
-dumb_sentence(item='hems')
