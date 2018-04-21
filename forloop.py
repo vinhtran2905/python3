f333321ec8e8e0492599d14281c760bea24c4429
Nguyen Tran <tranvinhnguyen@gmail.com>
2018-04-21 11:01:02 -0500

    for f in foods:

---
+++ forloop.py
@@ -0,0 +1,12 @@
+foods = ['bacon', 'tuna', 'ham', 'sausage', 'beef']
+
+for f in foods:
+    print(f)
+    print(len(f))
+    print(len(foods))
+print("\ntotal items in foods")
+print(len(foods))
+
+
+for f in foods[:2]:
+    print(f)
