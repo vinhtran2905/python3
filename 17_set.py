8e4531ba42a13d37734efd5a3daa2f9f6c3e3948
Nguyen Tran <tranvinhnguyen@gmail.com>
2018-04-21 12:14:21 -0500

    set {}

---
+++ 17_set.py
@@ -0,0 +1,9 @@
+#set using {}, list uses []
+groceries = {'cereal', 'milk', 'starcrunch', 'beer', 'duct tape', 'lotion', 'beer'}
+
+print(groceries)
+
+if 'milk' in groceries:
+    print("\nyou already have milk hoss!")
+else:
+    print("you need milk")
