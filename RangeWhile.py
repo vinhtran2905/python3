9a9831bbd9fe0313c15f329818b2d787315be6cc
Nguyen Tran <tranvinhnguyen@gmail.com>
2018-04-21 11:07:49 -0500

    range(a,b,in):
    while a < 10:
        print(something)
        a += 1

---
+++ RangeWhile.py
@@ -0,0 +1,18 @@
+for x in range(10):
+    print(x)
+    print('python is awsome')
+
+
+for y in range(5,20):
+    print(y)
+
+print("new range with 3 parameter: start, stop, inc")
+for z in range(10,40,10):
+    print(z)
+
+print('try with while loop')
+buttcrack = 5
+while buttcrack < 10:
+    print(buttcrack)
+    #buttcrack = buttcrack + 1
+    buttcrack += 1
