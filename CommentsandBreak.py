0fdfd93ee97a3c39c6e730860a8a6f17ec0eb645
Nguyen Tran <tranvinhnguyen@gmail.com>
2018-04-21 11:15:33 -0500

    comments

---
+++ CommentsandBreak.py
@@ -0,0 +1,18 @@
+magicNumber = 26
+
+#This program is examine how to use break and comments
+
+'''
+multiline comments
+
+'''
+"""
+multiple lines comments
+"""
+for n in range(101):
+    if n is magicNumber:
+        print(n, " is the magic number")
+    else:
+        print(n)
+print(9,"Bucky")
+
