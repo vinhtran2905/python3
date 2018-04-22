3d5f8e68fa26b46d459ea6bdcf9a393737982ae0
Vinh Tran <vinhtran2905@gmail.com>
2018-04-22 17:37:13 -0500

    update number

--- 01_numbers.py
+++ 01_numbers.py
@@ -10,3 +10,23 @@ print(c)
 d=6/4
 print(d)
 
+number = 3
+print(type(number))
+
+number = 3.5
+print(type(number))
+
+print(number == 1)
+
+print(number >= 2)
+
+print(number != 3.5)
+print(number <=2)
+
+
+num1 = '100'
+num2='200'
+print(num1+ num2)
+
+print(int(num1)+ int(num2))
+
