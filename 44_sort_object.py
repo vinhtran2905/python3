8f8ea929c599e3f9c1325953dfd50b82cb259387
Vinh Tran <vinhtran2905@gmail.com>
2018-04-22 14:14:19 -0500

    for user in sorted(users,key=attrgetter('user_id')):     print(user)

---
+++ 44_sort_object.py
@@ -0,0 +1,31 @@
+from operator import attrgetter
+
+class User:
+    def __init__(self, x,y):
+        self.name = x
+        self.user_id = y;
+
+    def __repr__(self):
+        return self.name + ':' + str(self.user_id)
+
+users = [
+    User('Bucky', 43),
+    User('Sally', 5),
+    User('Tuna', 61),
+    User('Brian', 2),
+    User('Joby', 77),
+    User('Amanda', 9),
+
+]
+
+for user in users:
+    print(user)
+
+
+print('--------------')
+for user in sorted(users,key=attrgetter('name')):
+    print(user)
+
+print('----by id----------')
+for user in sorted(users,key=attrgetter('user_id')):
+    print(user)
